423a2c4f816ab26a0e040778b68f23aab857b225
Actat <akio.toyosh@gmail.com>
2021-11-10 16:21:36 +0900

    test.htmlにremote fileの入力欄を作った

--- test.js
+++ test.js
@@ -1,15 +1,35 @@
-function test() {
-    const chr = document.forms.form.chrname.value;
-    const start = document.forms.form.start.value;
-    const end = document.forms.form.end.value;
-    const cram = document.forms.form.cram.files[0];
-    const crai = document.forms.form.crai.files[0];
+function testLocal() {
+    const chr = document.forms.form.chrnameLocal.value;
+    const start = document.forms.form.startLocal.value;
+    const end = document.forms.form.endLocal.value;
+    const cram = document.forms.form.cramLocal.files[0];
+    const crai = document.forms.form.craiLocal.files[0];
     console.log(chr);
     console.log(start);
     console.log(end)
     console.log(cram);
     console.log(crai);
     var c = new Cram(cram, crai, true);
+    c.getRecords(chr, start, end).then((reads) => {
+        reads.forEach(r => {
+            console.log(r);
+        })
+        console.log("finished.");
+    });
+}
+
+function testRemote() {
+    const chr = document.forms.form.chrnameRemote.value;
+    const start = document.forms.form.startRemote.value;
+    const end = document.forms.form.endRemote.value;
+    const cram = document.forms.form.cramRemote.files[0];
+    const crai = document.forms.form.craiRemote.files[0];
+    console.log(chr);
+    console.log(start);
+    console.log(end)
+    console.log(cram);
+    console.log(crai);
+    var c = new Cram(cram, crai, false);
     c.getRecords(chr, start, end).then((reads) => {
         reads.forEach(r => {
             console.log(r);
