a93649c501b4f3702ccefe2bc49a73f45318b369
Actat <akio.toyosh@gmail.com>
2021-12-08 11:58:54 +0900

    cramFileをfileHandlerとcramStreamに分割

---
+++ fileHandler.js
@@ -0,0 +1,29 @@
+class FileHandler {
+    constructor(file /* instance of File object or String of URL */, localFlag = true) {
+        this._file = file;
+        this._localFlag = localFlag;
+    }
+
+    load(pos, length) {
+        return new Promise((resolve, reject) => {
+            if (this._localFlag) {
+                var sliced = this._file.slice(pos, pos + length);
+                resolve(sliced.arrayBuffer());
+            } else {
+                var oReq = new XMLHttpRequest();
+                oReq.open("GET", this._file);
+                oReq.setRequestHeader("Range", "bytes=" + pos + "-" + (pos + length - 1));
+                oReq.responseType = "arraybuffer";
+                oReq.onload = function (oEvent) {
+                    const ab = oReq.response;
+                    if (ab) {
+                        resolve(ab);
+                    } else {
+                        reject(oReq.statusText);
+                    }
+                }
+                oReq.send();
+            }
+        });
+    }
+}
