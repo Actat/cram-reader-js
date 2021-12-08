8643e5557f077187c51ebb9c9c3b84ec32e64032
Actat <akio.toyosh@gmail.com>
2021-12-08 15:03:50 +0900

    FileHandler.load()でファイル全体を一括読み込み可能になった

--- fileHandler.js
+++ fileHandler.js
@@ -7,18 +7,24 @@ class FileHandler {
         this._localFlag = localFlag;
     }
 
-    load(pos, length) {
+    load(pos = -1, length = -1) {
         return new Promise((resolve, reject) => {
             if (this._localFlag) {
-                var sliced = this._file.slice(pos, pos + length);
-                resolve(sliced.arrayBuffer());
+                if (pos >= 0 && length > 0) {
+                    var sliced = this._file.slice(pos, pos + length);
+                    resolve(sliced.arrayBuffer());
+                } else {
+                    resolve(this._file.arrayBuffer());
+                }
             } else {
                 var oReq = new XMLHttpRequest();
                 oReq.open("GET", this._file);
-                oReq.setRequestHeader(
-                    "Range",
-                    "bytes=" + pos + "-" + (pos + length - 1)
-                );
+                if (pos >= 0 && length > 0) {
+                    oReq.setRequestHeader(
+                        "Range",
+                        "bytes=" + pos + "-" + (pos + length - 1)
+                    );
+                }
                 oReq.responseType = "arraybuffer";
                 oReq.onload = function (oEvent) {
                     const ab = oReq.response;
