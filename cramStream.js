ecebd874ca832324519277b94f199d20c95c94f3
Actat <akio.toyosh@gmail.com>
2021-12-15 12:02:55 +0900

    arraybufferの長さはlengthではなくbyteLength

--- cramStream.js
+++ cramStream.js
@@ -7,7 +7,7 @@ class CramStream {
   concat(arrbuf) {
     var u8a = new Uint8Array(this.buffer_.byteLength + arrbuf.byteLength);
     u8a.set(new Uint8Array(this.buffer_), 0);
-    u8a.set(new Uint8Array(arrbuf), this.buffer_.length);
+    u8a.set(new Uint8Array(arrbuf), this.buffer_.byteLength);
     this.buffer_ = u8a.buffer;
   }
 
