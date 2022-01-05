9795558f5d0e50b593aa2bd73b109e3633e55c81
Actat <akio.toyosh@gmail.com>
2022-01-05 13:30:16 +0900

    CramRansの整理

--- cramRans.js
+++ cramRans.js
@@ -1,35 +1,35 @@
 class CramRans {
-  constructor(input) {
-    this.input = new CramStream(input);
+  constructor(/*ArrayBuffer*/ input) {
+    this.input_ = new CramStream(input);
   }
 
   ransDecode() {
-    const order = this.input.readInt8();
-    const n_in = this.input.readUint32();
-    const n_out = this.input.readUint32();
+    const order = this.input_.readInt8();
+    const n_in = this.input_.readUint32();
+    const n_out = this.input_.readUint32();
     var output = new Uint8Array(n_out);
     if (order == 0) {
-      this.ransDecode0(output, n_out);
+      this.ransDecode0_(output, n_out);
     } else {
-      this.ransDecode1(output, n_out);
+      this.ransDecode1_(output, n_out);
     }
     return output.buffer;
   }
 
-  readFrequencies0(F, C) {
-    var sym = this.input.readUint8();
+  readFrequencies0_(F, C) {
+    var sym = this.input_.readUint8();
     var last_sym = sym;
     var rle = 0;
     while (true) {
-      var f = this.input.readItf8();
+      var f = this.input_.readItf8();
       F[sym] = f;
       if (rle > 0) {
         rle -= 1;
         sym = sym + 1;
       } else {
-        sym = this.input.readUint8();
+        sym = this.input_.readUint8();
         if (sym == last_sym + 1) {
-          rle = this.input.readUint8();
+          rle = this.input_.readUint8();
         }
       }
       last_sym = sym;
@@ -43,11 +43,11 @@ class CramRans {
     }
   }
 
-  ransGetCumulativeFreq(R) {
+  ransGetCumulativeFreq_(R) {
     return R & 0xfff;
   }
 
-  ransGetSymbolFromFreq(C, f) {
+  ransGetSymbolFromFreq_(C, f) {
     var s = 0;
     while (s + 1 < C.length && f >= C[s + 1]) {
       s += 1;
@@ -55,24 +55,24 @@ class CramRans {
     return s;
   }
 
-  ransAdvanceStep(R, c, f) {
+  ransAdvanceStep_(R, c, f) {
     return f * (R >> 12) + (R & 0xfff) - c;
   }
 
-  ransRenorm(R) {
+  ransRenorm_(R) {
     while (R < 1 << 23) {
-      R = (R << 8) + this.input.readUint8();
+      R = (R << 8) + this.input_.readUint8();
     }
     return R;
   }
 
-  ransDecode0(output, nbytes) {
+  ransDecode0_(output, nbytes) {
     var F = new Array(256).fill(0);
     var C = new Array(256).fill(0);
     var R = new Array(4).fill(0);
-    this.readFrequencies0(F, C);
+    this.readFrequencies0_(F, C);
     for (var j = 0; j < 4; j++) {
-      R[j] = this.input.readUint32();
+      R[j] = this.input_.readUint32();
     }
     var i = 0;
     while (i < nbytes) {
@@ -80,29 +80,29 @@ class CramRans {
         if (i + j >= nbytes) {
           return;
         }
-        var f = this.ransGetCumulativeFreq(R[j]);
-        var s = this.ransGetSymbolFromFreq(C, f);
+        var f = this.ransGetCumulativeFreq_(R[j]);
+        var s = this.ransGetSymbolFromFreq_(C, f);
         output[i + j] = s;
-        R[j] = this.ransAdvanceStep(R[j], C[s], F[s]);
-        R[j] = this.ransRenorm(R[j]);
+        R[j] = this.ransAdvanceStep_(R[j], C[s], F[s]);
+        R[j] = this.ransRenorm_(R[j]);
       }
       i += 4;
     }
   }
 
-  readFrequencies1(F, C) {
-    var sym = this.input.readUint8();
+  readFrequencies1_(F, C) {
+    var sym = this.input_.readUint8();
     var last_sym = sym;
     var rle = 0;
     while (true) {
-      this.readFrequencies0(F[sym], C[sym]);
+      this.readFrequencies0_(F[sym], C[sym]);
       if (rle > 0) {
         rle = rle - 1;
         sym = sym + 1;
       } else {
-        sym = this.input.readUint8();
+        sym = this.input_.readUint8();
         if (sym == last_sym + 1) {
-          rle = this.input.readUint8();
+          rle = this.input_.readUint8();
         }
       }
       last_sym = sym;
@@ -112,35 +112,35 @@ class CramRans {
     }
   }
 
-  ransDecode1(output, nbytes) {
+  ransDecode1_(output, nbytes) {
     var F = Array.from(new Array(256), () => new Array(256).fill(0));
     var C = Array.from(new Array(256), () => new Array(256).fill(0));
     var R = new Array(4).fill(0);
     var L = new Array(4).fill(0);
-    this.readFrequencies1(F, C);
+    this.readFrequencies1_(F, C);
     for (var j = 0; j < 4; j++) {
-      R[j] = this.input.readUint32();
+      R[j] = this.input_.readUint32();
       L[j] = 0;
     }
     var i = 0;
     while (i < nbytes / 4) {
       for (var j = 0; j < 4; j++) {
-        var f = this.ransGetCumulativeFreq(R[j]);
-        var s = this.ransGetSymbolFromFreq(C[L[j]], f);
+        var f = this.ransGetCumulativeFreq_(R[j]);
+        var s = this.ransGetSymbolFromFreq_(C[L[j]], f);
         output[i + Math.floor((j * nbytes) / 4)] = s;
-        R[j] = this.ransAdvanceStep(R[j], C[L[j]][s], F[L[j]][s]);
-        R[j] = this.ransRenorm(R[j]);
+        R[j] = this.ransAdvanceStep_(R[j], C[L[j]][s], F[L[j]][s]);
+        R[j] = this.ransRenorm_(R[j]);
         L[j] = s;
       }
       i += 1;
     }
     i *= 4;
     while (i < nbytes) {
-      f = this.ransGetCumulativeFreq(R[3]);
-      s = this.ransGetSymbolFromFreq(C[L[3]], f);
+      f = this.ransGetCumulativeFreq_(R[3]);
+      s = this.ransGetSymbolFromFreq_(C[L[3]], f);
       output[i + Math.floor((3 * nbytes) / 4)] = s;
-      R[3] = this.ransAdvanceStep(R[3], C[L[3]][s], F[L[3]][s]);
-      R[3] = this.ransRenorm(R[3]);
+      R[3] = this.ransAdvanceStep_(R[3], C[L[3]][s], F[L[3]][s]);
+      R[3] = this.ransRenorm_(R[3]);
       L[3] = s;
       i += 1;
     }
