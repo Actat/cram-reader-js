5b6d99b9731c4699f3fbdaee1df37f527da998c0
Actat <akio.toyosh@gmail.com>
2022-01-19 14:27:24 +0900

    concatenate and minify the files

---
+++ concat.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cat src/bitsIo.js > cram-reader-js.js
+cat src/cram.js >> cram-reader-js.js
+cat src/cramContainer.js >> cram-reader-js.js
+cat src/cramDataContainer.js >> cram-reader-js.js
+cat src/cramHeader.js >> cram-reader-js.js
+cat src/cramRans.js >> cram-reader-js.js
+cat src/cramRecord.js >> cram-reader-js.js
+cat src/cramSlice.js >> cram-reader-js.js
+cat src/cramStream.js >> cram-reader-js.js
+cat src/fileHandler.js >> cram-reader-js.js
