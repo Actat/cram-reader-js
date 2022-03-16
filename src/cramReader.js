aefd8f6d6055e58cf5b40e2b85b7e3243627a12c
Actat <akio.toyosh@gmail.com>
2022-03-16 15:00:01 +0900

    web workerのpostMessageで関数を呼び分ける

--- src/cramReader.js
+++ src/cramReader.js
@@ -6,21 +6,45 @@ class CramReader {
     if (!cram || !crai) {
       throw "Files are Falsy";
     }
-    this.cram_ = new Cram(cram, crai, local_flag, fa, fai);
+    this.listeners_ = Map();
     this.worker_ = new Worker("cram-reader-worker.min.js");
+    this.worker_.onmessage = function (event) {
+      var callbackfunc = this.listeners_.get(event.data[0]);
+      this.listeners_.delete(event.data[0]);
+      if (callbackfunc) {
+        callbackfunc(event.data[1]);
+      }
+    };
+    this.sendQuery_("init", [cram, crai, local_flag, fa, fai], undefined);
   }
 
-  getRecords(chr, start, end, callback, onerror) {
+  getRecords(chr, start, end, callback) {
     if (callback) {
       this.worker_.onmessage = function (result) {
         callback(result);
       };
     }
-    if (onerror) {
-      this.worker_.onerror = function (error) {
-        onerror(error);
-      };
+    this.sendQuery_("read", [chr, start, end], callback);
+  }
+
+  sendQuery_(fname, args, callback) {
+    var uuid = this.generateUUID4_();
+    this.listeners_.set(uuid, callback);
+    this.worker_.postMessage([uuid, fname, args]);
+  }
+
+  generateUUID4_() {
+    var format = "RRRRRRRR-RRRR-4RRR-rRRR-RRRRRRRRRRRR";
+    for (var i = 0; i < format.length; i++) {
+      switch (format[i]) {
+        case "R":
+          format[i] = Math.floor(Math.random() * 16).toString(16);
+          break;
+        case "r":
+          format[i] = (Math.floor(Math.random() * 4) + 8).toString(16);
+          break;
+      }
     }
-    this.worker_.postMessage([this.cram_, chr, start, end]);
+    return format.join();
   }
 }
--- src/worker.js
+++ src/worker.js
@@ -1,19 +1,33 @@
-onmessage = function (e) {
-  var worker_result = e[0]
-    .getRecords(e[1], e[2], e[3])
-    .then((reads) => {
-      var result = new String();
-      reads.forEach((r) => {
-        console.log(r);
-        result += r.toSAMString() + "\n";
-      });
-      changeState(result);
-      console.log(result);
-      console.log("finished.");
-    })
-    .catch((reason) => {
-      console.log(reason);
-      changeState("Error occurred. (" + reason + ")");
-    });
-  postMessage(worker_result);
+var cram = undefined;
+
+onmessage = function (data) {
+  var uuid = data[0];
+  var fname = data[1];
+  var args = data[2];
+
+  switch (fname) {
+    case "init":
+      cram = new Cram(args[0], args[1], args[2], args[3], args[4]);
+      postMessage([uuid]);
+      break;
+    case "read":
+      var reads = cram
+        .getRecords(args[0], args[1], args[2])
+        .then((reads) => {
+          var result = new String();
+          reads.forEach((r) => {
+            console.log(r);
+            result += r.toSAMString() + "\n";
+          });
+          changeState(result);
+          console.log(result);
+          console.log("finished.");
+        })
+        .catch((reason) => {
+          console.log(reason);
+          changeState("Error occurred. (" + reason + ")");
+        });
+      postMessage([uuid, reads]);
+      break;
+  }
 };
